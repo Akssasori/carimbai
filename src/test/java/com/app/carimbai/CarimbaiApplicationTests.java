0e7a4d73b5d4320b957989ede9442dbbce1da777
Lucas Diniz <205810771+lucas-diniz_globogc@users.noreply.github.com>
2025-11-09 11:13:28 -0300

    frist commit

---
+++ src/test/java/com/app/carimbai/CarimbaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.carimbai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarimbaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
