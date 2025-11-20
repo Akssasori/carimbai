3f3a1514245bd973867a623d284176db2a4e26e2
Lucas Diniz <205810771+lucas-diniz_globogc@users.noreply.github.com>
2025-11-20 13:00:17 -0300

    test railway two

--- src/main/java/com/app/carimbai/CarimbaiApplication.java
+++ src/main/java/com/app/carimbai/CarimbaiApplication.java
@@ -1,10 +1,20 @@
 package com.app.carimbai;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+import java.util.Arrays;
+
+@Slf4j
 @SpringBootApplication
 public class CarimbaiApplication {
+
+    public CarimbaiApplication(Environment env) {
+        log.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+        log.info("DB_URL: {}", env.getProperty("DB_URL"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarimbaiApplication.class, args);
