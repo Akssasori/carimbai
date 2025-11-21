035eb00d03fa93dad2098502b253805787badc64
Lucas Diniz <205810771+lucas-diniz_globogc@users.noreply.github.com>
2025-11-21 13:45:48 -0300

    test envs log two ens

--- src/main/java/com/app/carimbai/CarimbaiApplication.java
+++ src/main/java/com/app/carimbai/CarimbaiApplication.java
@@ -14,6 +14,7 @@ public class CarimbaiApplication {
     public CarimbaiApplication(Environment env) {
         log.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
         log.info("DB_URL_DEV: {}", env.getProperty("DB_URL_DEV"));
+        log.info("DB_URL: {}", env.getProperty("DB_URL"));
         log.info("DB_USERNAME: {}", env.getProperty("DB_USERNAME"));
         log.info("DB_PASSWORD: {}", env.getProperty("DB_PASSWORD"));
     }
