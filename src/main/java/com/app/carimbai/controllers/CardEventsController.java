a97e185fe7f42b9e376abe763312354b6a4b9ee4
Akssasori <diniz3003@gmail.com>
2026-04-11 08:06:15 -0300

    Enhance CardEventsController to send initial SSE event on subscription

--- src/main/java/com/app/carimbai/controllers/CardEventsController.java
+++ src/main/java/com/app/carimbai/controllers/CardEventsController.java
@@ -2,6 +2,7 @@ package com.app.carimbai.controllers;
 
 import com.app.carimbai.sse.SseEmitterRegistry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/cards")
 @RequiredArgsConstructor
+@Slf4j
 public class CardEventsController {
 
     private final SseEmitterRegistry registry;
@@ -23,6 +27,14 @@ public class CardEventsController {
     @GetMapping(value = "/{cardId}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@PathVariable Long cardId) {
         long timeoutMs = (qrTtlSeconds + 10) * 1000L;
-        return registry.register(cardId, timeoutMs);
+        SseEmitter emitter = registry.register(cardId, timeoutMs);
+
+        try {
+            emitter.send(SseEmitter.event().name("connected").data("{\"status\":\"ok\"}"));
+        } catch (IOException e) {
+            log.debug("Failed to send initial SSE event for cardId={}", cardId);
+        }
+
+        return emitter;
     }
 }
